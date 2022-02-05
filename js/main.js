adf433be3ab369ba102983f8980a093492798ee6
SiyoonJung <highlight9111@gmail.com>
2022-02-05 23:28:58 +0900

    멤버: 레이아웃 수정

--- js/main.js
+++ js/main.js
@@ -46,3 +46,31 @@ $(".prodMenu ul li").click(function () {
   });
   return false;
 });
+/* ----- Typing Effect ----- */
+function randomiseDelay(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
+/* ----- Typing Effect ----- */
+
+function randomiseDelay(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function typeWrite(el){
+  el.addClass('cursor')
+  var text = el.data('string');
+  var delay = parseInt(el.data('delay'));
+  var randInt = 0;
+  for (var i = 0; i < text.length; i++) {
+    randInt += parseInt(randomiseDelay(50,100));
+    var typing = setTimeout(function(y){
+      el.append(text.charAt(y));
+    },randInt, i);
+  };
+}
+
+typeWrite($(".title"));
